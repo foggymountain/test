d41790daa336a51940868fa86ccac483d94ebe6d
FoggyMountain <36545948+foggymountain@users.noreply.github.com>
2018-07-18 12:57:37 -0600

    Add files via upload

---
+++ DjenkApplication.java
@@ -0,0 +1,12 @@
+package com.ojw.djenk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjenkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjenkApplication.class, args);
+	}
+}
